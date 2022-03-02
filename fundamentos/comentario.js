6eda42f624fe9dbbc5a046a2e79afad47f5a05c8
geancarlosgmartins@hotmail.com <geancarlosgmartins@hotmail.com>
2022-03-02 18:09:44 -0300

    Javascript:Fundamentos Aula:7

---
+++ fundamentos/comentario.js
@@ -0,0 +1,16 @@
+// Comentario de uma linha
+console.log('Linha 1')
+
+/*
+Comentario de 
+multiplas linhas
+*/
+
+console.log('Linha 2')
+
+/*
+* Comentário de 
+* multriplas linhas
+*/
+
+console.log('Linha 3')
