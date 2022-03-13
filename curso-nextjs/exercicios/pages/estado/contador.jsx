61dffe4c29fa7d2626e19204edefede69b7c79ca
geancarlosgmartins@hotmail.com <geancarlosgmartins@hotmail.com>
2022-03-12 23:07:49 -0300

    Revisão React Aula:37

--- curso-nextjs/exercicios/pages/estado/contador.jsx
+++ curso-nextjs/exercicios/pages/estado/contador.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
+import ContadorDisplay from "../../components/ContadorDisplay"
+
 export default function contador(){
+    const [numero,setNumero] = useState(0)
+    
+    
+    const dec = () => setNumero(numero - 1)
+    
     return(
-        <div>
+        <div style={{
+            display: "flex",
+            flexDirection:"column",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
             <h1>Contador</h1>
-            <div>Valor</div>
-            <button>-</button>
-            <button>+</button>
+            <ContadorDisplay numero={numero}/>
+            
+
+            <div>
+            <button onClick={dec}>-</button>
+            <button onClick={() => setNumero(numero + 1)}>+</button>
+            </div>
         </div>
     )
 }
