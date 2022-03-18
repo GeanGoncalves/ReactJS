1a02b6ea3e0d35dbb0a71a6a52bfeed926b2d057
geancarlosgmartins@hotmail.com <geancarlosgmartins@hotmail.com>
2022-03-18 15:42:25 -0300

    Entendendo TypeScript Aula:18

--- curso-typescript/interfaces/interfaces.ts
+++ curso-typescript/interfaces/interfaces.ts
@@ -44,3 +44,21 @@ meuCliente.nome = 'Han'
 saudarComOla(meuCliente)
 meuCliente.saudar('Solo')
 console.log(meuCliente.ultimaCompra)
+
+
+// Interface Função
+interface FuncaoCalculo{
+    (a: number, b:number): number
+}
+
+let potencia: FuncaoCalculo
+
+potencia = function(base: number, exp: number): number{
+    //Math.pow(3,10)
+    //3**10
+    return Array(exp).fill(base).reduce((t,a) => t * a)
+}
+
+console.log(potencia(3,10))
+console.log(Math.pow(3,10))
+console.log(3 ** 10)
