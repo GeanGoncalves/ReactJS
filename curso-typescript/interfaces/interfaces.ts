bceb180c961550f82d230764915f85ace2ad90f7
geancarlosgmartins@hotmail.com <geancarlosgmartins@hotmail.com>
2022-03-18 15:32:02 -0300

    Entendendo TypeScript Aula:16

--- curso-typescript/interfaces/interfaces.ts
+++ curso-typescript/interfaces/interfaces.ts
@@ -2,6 +2,7 @@ interface Humano{
     nome: string
     idade?:number
     [prop: string] : any
+    saudar(sobrenome: string) :void
 }
 
 function saudarComOla(pessoa: Humano){
@@ -13,12 +14,17 @@ function mudarNome(pessoa: Humano){
 }
 
 
-const pessoa = {
+const pessoa: Humano = {
     nome: 'João',
-    
+    idade: 27,
+    saudar(sobrenome: string){
+        console.log('Olá, meu nome é '
+        + this.nome + ' ' + sobrenome)
+    }
 }
 
 saudarComOla(pessoa)
 mudarNome(pessoa)
 saudarComOla(pessoa)
-saudarComOla({nome:'Jonas', idade: 27, altura: 1.75})
+//saudarComOla({nome:'Jonas', idade: 27, altura: 1.75})
+pessoa.saudar('Skywalker')
