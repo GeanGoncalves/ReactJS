c87c04645c10ba52134cf76891965309d5510959
geancarlosgmartins@hotmail.com <geancarlosgmartins@hotmail.com>
2022-03-19 14:52:14 -0300

    Projeto Porta Premiada - Aula:13

--- curso-nextjs/portas/pages/index.jsx
+++ curso-nextjs/portas/pages/index.jsx
@@ -4,8 +4,9 @@ import Porta from "../components/Porta";
 export default function Home() {
   return (
     <div style={{display: "flex"}}>
-     <Porta />
-     <Porta />
+     <Porta selecionada={true} />
+     <Porta selecionada={false}/>
+     
     </div>
   )
 }
